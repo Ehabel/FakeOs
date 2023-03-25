4cc9cc70c4e63fa837325453dd55eaa2bdf782fd
Ehab El Cheikh <eelcheikh@hotmail.com>
2023-03-25 18:13:30 +1100

    Added more menu stuff

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,49 @@
 const startBtn = document.querySelector(".taskbar__start__button");
 const menuApp = document.querySelector(".menu");
 let isMenuOpen = false;
+const menuHTML = `
+<div class="menu__header">
+<div class="menu__header__contents">
+    <img
+        src="./assets/ball.jpg"
+        class="menu__header__image"
+    />
+    <p class="menu__header__user">USER</p>
+</div>
+</div>
+<div class="menu__mid">
+<div class="menu__mid__contents">
+    <div class="menu__mid__contents__left">
+        <div class="menu__mid__contents__left__calc">
+            <img
+                src="./assets/cal.png"
+                class="menu__mid__contents__left__calc__image"
+            />
+            <p
+                class="menu__mid__contents__left__calc__text"
+            >
+                Calculator
+            </p>
+        </div>
+    </div>
+    <div class="menu__mid__contents__right">
+        <div class="menu__mid__contents__right__calc">
+            <img
+                src="./assets/cal.png"
+                class="menu__mid__contents__right__calc__image"
+            />
+            <p
+                class="menu__mid__contents__right__calc__text"
+            >
+                Calculator
+            </p>
+        </div>
+    </div>
+</div>
+</div>
+<div class="menu__footer">
+<div class="menu__footer__contents"></div>
+</div>`;
 
 function updateClock() {
     let now = new Date();
@@ -15,14 +58,12 @@ const createMenuHTML = () => {
     divTag.classList.add("menu__container");
     menuApp.appendChild(divTag);
     const menuChild = document.getElementsByClassName("menu__container")[0];
-    menuChild.innerHTML += `
-        <div class="menu__header">
-            <img src="./assets/ball.jpg" class="menu__header__image" />
-        </div>`;
+    menuChild.innerHTML += menuHTML;
 };
 
 startBtn.addEventListener("click", () => {
     const menuChild = document.getElementsByClassName("menu__container")[0];
+    // menuApp.classList.toggle("menu--show");
     if (!isMenuOpen) {
         startBtn.src = "./assets/buttons-3.png";
         createMenuHTML();
